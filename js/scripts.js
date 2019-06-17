3a43ac0894a9b0f3c8f5c4600918230e89ede2c7
Alexander Sergejev and Marina Poltorak <marina@>
2019-06-17 10:16:32 -0700

    Add initial files and folders, with business end functionality

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function Place(name, location, landmarks, food, notes, image) {
+  this.name = name;
+  this.location = location;
+  this.landmarks = landmarks;
+  this.food = food;
+  this.notes = notes;
+  this.image = image;
+}
+
+Place.prototype.addPlace = function (place) {
+  this.place.push(place);
+};
+
+Place.prototype.paragraphDisplay = function () {
+  return this.name + ' is located in ' + this.location + '.' + ' ' + this.notes;
+};
+
+var Korea = new Place('Gyeonggi-Do', 'Korea', ['Surisan', 'Anyang', 'Itaewon'], ['samgyupsal', 'kimchi bokkeumbap', 'Maeuntang'], 'Gyeonggi-Do is a province of Korea. There are many excellent things to see and experience here. Half of Korean cuisine consists of various delicious and hearty soups and stews. Start a your day right with a Seafood stew. Continue with an invigorating hike in beautiful Suri moauntian. Relax in one of the numerous spas, known as Jjimjilbang. Finish the day with a delicious meal of Korean BBQ. Make sure to visit Itaewon - known locally as International National Town - before you leave.');
+
+var LookoutTower = new Place('Indian Ridge Lookout Tower', 'Oregon', ['Volcanos', 'Lakes', 'Flowers', 'Old Growth Forest'], ['Your own camp made food'], 'The roads are full of potholes and washouts, only passable by high clearence vehicles');
