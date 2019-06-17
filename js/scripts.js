5bfdb9fa558b83b521335148fca0b755194b9f05
Alexander Sergejev and Marina Poltorak <marina@>
2019-06-17 12:01:02 -0700

    add image

--- js/scripts.js
+++ js/scripts.js
@@ -12,25 +12,33 @@ Place.prototype.addPlace = function (place) {
 };
 
 Place.prototype.paragraphDisplay = function () {
-  return this.name + ' is located in ' + this.location + '.' + ' ' + this.notes;
-};
+  return this.name + ' is located in ' + this.location + '.' + ' ' + this.notes + + " " +
+  this.image;
+  };
 
-var Korea = new Place('Gyeonggi-Do', 'Korea', ['Surisan', 'Anyang', 'Itaewon'], ['samgyupsal', 'kimchi bokkeumbap', 'Maeuntang'], 'Gyeonggi-Do is a province of Korea. There are many excellent things to see and experience here. Half of Korean cuisine consists of various delicious and hearty soups and stews. Start a your day right with a Seafood stew. Continue with an invigorating hike in beautiful Suri moauntian. Relax in one of the numerous spas, known as Jjimjilbang. Finish the day with a delicious meal of Korean BBQ. Make sure to visit Itaewon - known locally as International National Town - before you leave.');
 
-var LookoutTower = new Place('Indian Ridge Lookout Tower', 'Oregon', ['Volcanos', 'Lakes', 'Flowers', 'Old Growth Forest'], ['Your own camp made food'], 'The roads are full of potholes and washouts, only passable by high clearence vehicles');
+var Korea = new Place('Gyeonggi-Do', 'Korea', ['Surisan', 'Anyang', 'Itaewon'], ['samgyupsal', 'kimchi bokkeumbap', 'Maeuntang'], 'Gyeonggi-Do is a province of Korea. There are many excellent things to see and experience here. Half of Korean cuisine consists of various delicious and hearty soups and stews. Start a your day right with a Seafood stew. Continue with an invigorating hike in beautiful Suri moauntian. Relax in one of the numerous spas, known as Jjimjilbang. Finish the day with a delicious meal of Korean BBQ. Make sure to visit Itaewon - known locally as International National Town - before you leave.',  ['<img src="img/suri1.jpg">','<img src="img/suri2.jpg">','<img src="img/suri3.jpg">']);
+
+var LookoutTower = new Place('Indian Ridge Lookout Tower', 'Oregon', ['Volcanos', 'Lakes', 'Flowers', 'Old Growth Forest'], ['Your own camp made food'], 'The roads are full of potholes and washouts, only passable by high clearence vehicles', ['<img src="img/tower.jpeg">']);
 
 // front end
 $(document).ready(function() {
   $('.formOne').submit(function(event) {
     event.preventDefault();
     console.log("here");
-    $('#display').text(Korea.paragraphDisplay());
+    // $('#display').text("");
+    // $('#kimage').text("");
+    $('#display').html(Korea.paragraphDisplay());
+    //$('#kimage').append(Korea.image);
   });
 });
 $(document).ready(function() {
   $('.formTwo').submit(function(event) {
     event.preventDefault();
     console.log("here");
-    $('#display').text(LookoutTower.paragraphDisplay());
+    // $('#display').text("");
+    // $('#kimage').text("");
+    $('#display').html(LookoutTower.paragraphDisplay());
+
   });
 });
