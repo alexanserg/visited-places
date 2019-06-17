2f22bd2c51d32557efeecca182f4750e89db2885
Alexander Sergejev and Marina Poltorak <alexander@>
2019-06-17 11:00:49 -0700

    add content and front end functionality to page

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,19 @@ Place.prototype.paragraphDisplay = function () {
 var Korea = new Place('Gyeonggi-Do', 'Korea', ['Surisan', 'Anyang', 'Itaewon'], ['samgyupsal', 'kimchi bokkeumbap', 'Maeuntang'], 'Gyeonggi-Do is a province of Korea. There are many excellent things to see and experience here. Half of Korean cuisine consists of various delicious and hearty soups and stews. Start a your day right with a Seafood stew. Continue with an invigorating hike in beautiful Suri moauntian. Relax in one of the numerous spas, known as Jjimjilbang. Finish the day with a delicious meal of Korean BBQ. Make sure to visit Itaewon - known locally as International National Town - before you leave.');
 
 var LookoutTower = new Place('Indian Ridge Lookout Tower', 'Oregon', ['Volcanos', 'Lakes', 'Flowers', 'Old Growth Forest'], ['Your own camp made food'], 'The roads are full of potholes and washouts, only passable by high clearence vehicles');
+
+// front end
+$(document).ready(function() {
+  $('.formOne').submit(function(event) {
+    event.preventDefault();
+    console.log("here");
+    $('#display').text(Korea.paragraphDisplay());
+  });
+});
+$(document).ready(function() {
+  $('.formTwo').submit(function(event) {
+    event.preventDefault();
+    console.log("here");
+    $('#display').text(LookoutTower.paragraphDisplay());
+  });
+});
